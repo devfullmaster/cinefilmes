2d0be6f3dcbab991eac47b8e52478d2b924162d1
Rudy Almeida <devfullmaster@gmail.com>
2022-10-03 20:28:42 -0300

    atualizacao

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+
+
+//https://sujeitoprogramador.com/r-api/?api=filmes/
+
 export default function App() {
   return (
     <div>
-      <h1>CineFilmes</h1>
+      <h1>CineFilmesReactJs</h1>
     </div>
   );
 }
---
+++ src/pages/Home/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>CineFilmes Home</h1>
+    </div>
+  );
+}
